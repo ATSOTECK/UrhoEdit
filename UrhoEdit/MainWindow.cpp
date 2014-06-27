86573bb6ac77c57433b77b01b7085f1b1b859907
Skyler Burwell <skyboy49707@gmail.com>
2014-06-26 17:03:27 -0700

    Added some debugging functions.

--- UrhoEdit/MainWindow.cpp
+++ UrhoEdit/MainWindow.cpp
@@ -3,6 +3,36 @@
 
 #include <QFile>
 #include <QTextStream>
+#include <QAction>
+#include <QFileDialog>
+#include <QDir>
+#include <QDebug>
+
+MainWindow *w;
+
+void setMainWindow(MainWindow *mw) {
+    w = mw;
+}
+
+void catchMessage(QtMsgType type, const QMessageLogContext &, const QString &msg) {
+    switch (type) {
+    case QtDebugMsg:
+        w->debug(msg);
+        break;
+    case QtWarningMsg:
+        w->warn(msg);
+        break;
+    case QtCriticalMsg:
+        w->crit(msg);
+        break;
+    case QtFatalMsg:
+        w->fail(msg);
+        break;
+    default:
+        break;
+    }
+}
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,19 +41,63 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     
     setupUi();
+    
+    connect(ui->actionOpen, SIGNAL(triggered()), this, SLOT(open()));
+    connect(ui->actionSave, SIGNAL(triggered()), this, SLOT(save()));
+    connect(ui->actionSave_as, SIGNAL(triggered()), this, SLOT(saveAs()));
+    connect(ui->actionRun, SIGNAL(triggered()), this, SLOT(runGame()));
+    
+    qInstallMessageHandler(catchMessage);
 }
 
 MainWindow::~MainWindow() {
     delete ui;
 }
 
+void MainWindow::debug(const QString &msg) {
+    QColor c(98, 217, 239);
+    ui->console->setTextColor(c);
+    ui->console->append(msg);
+    ui->console->setTextColor(Qt::white);
+}
+void MainWindow::warn(const QString &msg) {
+    ui->console->setTextColor(QColor(230, 219, 116));
+    ui->console->append(msg);
+    ui->console->setTextColor(Qt::white);
+}
+
+void MainWindow::crit(const QString &msg) {
+    ui->console->setTextColor(Qt::red);
+    ui->console->append(msg);
+    ui->console->setTextColor(Qt::white);
+}
+
+void MainWindow::fail(const QString &msg) {
+    ui->console->setTextColor(Qt::red);
+    ui->console->append(msg);
+    ui->console->setTextColor(Qt::white);
+}
+
 void MainWindow::setupUi() {
-    //Don't show that annoying blue rectangle
+    setCorner(Qt::BottomLeftCorner, Qt::LeftDockWidgetArea);
+    setCorner(Qt::BottomRightCorner, Qt::RightDockWidgetArea);
+    setCorner(Qt::TopLeftCorner, Qt::LeftDockWidgetArea);
+    setCorner(Qt::TopRightCorner, Qt::RightDockWidgetArea);
+    
+    //Don't show that annoying blue rectangle on mac
     ui->resourcesTree->setAttribute(Qt::WA_MacShowFocusRect, false);
     ui->resourcesTree->setHeaderHidden(true);
     
     ui->resourcesDockWidgetContents->layout()->setContentsMargins(0, 0, 0, 0);
+    ui->consoleDockWidgetContents->layout()->setContentsMargins(0, 0, 0, 0);
     
+    
+    QIcon runIcon(":/Resources/Images/run.png");
+    QAction *actionRun = new QAction(runIcon, tr("Run"), this);
+    
+    connect(actionRun, SIGNAL(triggered()), this, SLOT(runGame()));
+    
+    ui->mainToolBar->addAction(actionRun);
     
     QFile *style;
     QString styleText;
@@ -39,4 +113,21 @@ void MainWindow::setupUi() {
     style->close();
     delete style;
     setStyleSheet(styleText);
+}
+
+void MainWindow::open() {
+    qDebug() << "path" << QDir::homePath() + "/Documents/";
+    QFileDialog::getOpenFileName(this, tr("Open"), QDir::homePath() + "/Documents/");
+}
+
+void MainWindow::save() {
+    
+}
+
+void MainWindow::saveAs() {
+    
+}
+
+void MainWindow::runGame() {
+    
 }
--- UrhoEdit/MainWindow.h
+++ UrhoEdit/MainWindow.h
@@ -14,10 +14,25 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     
+    void debug(const QString &msg);
+    void warn(const QString &msg);
+    void crit(const QString &msg);
+    void fail(const QString &msg);
+    
     void setupUi();
+    
+public slots:
+    void open();
+    void save();
+    void saveAs();
+    
+    void runGame();
     
 private:
     Ui::MainWindow *ui;
 };
+
+void setMainWindow(MainWindow *mw);
+void catchMessage(QtMsgType type, const QMessageLogContext &, const QString &msg);
 
 #endif // MAINWINDOW_H
